d6ed577b2b174dce10aca097053d518f89da697d
chris260602 <christoperlim20@gmail.com>
2022-10-11 10:46:19 +0700

    Adding Sitemap

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+window.addEventListener("load", (event) => {
+  console.log("page is fully loaded");
+});
 root.render(
   <BrowserRouter>
     <App />
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,13 @@
-import { useRoutes } from "react-router-dom";
+import { Navigate, useRoutes } from "react-router-dom";
 import Home from "../views/Home/Home";
 import NotFound from "../views/NotFound/NotFound";
 import Portfolio from "../views/Portfolio/Portfolio";
-import Welcome from "../views/Welcome/Welcome";
+
 const MainRoute = () => {
   return useRoutes([
     {
       path: "/",
-      element: <Home />,
+      element: <Navigate to={"/Home"} />,
     },
     {
       path: "/Home",
