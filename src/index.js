2ae8a1672d7a5910b54f66fcac36ead6cf1dcde5
chris260602 <christoperlim20@gmail.com>
2022-10-19 20:04:49 +0700

    Change CV & Darken Modal

--- src/index.js
+++ src/index.js
@@ -5,9 +5,7 @@ import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-window.addEventListener("load", (event) => {
-  console.log("page is fully loaded");
-});
+
 root.render(
   <BrowserRouter>
     <App />
